a9d09440de6e9b0f4ff56bab36ea37d7c013cf67
mohan <mohat commitn@MOHAN>
2025-08-31 21:39:47 +0530

    my first commit msg

---
+++ TestingMoni/src/main/java/moni/Testing.java
@@ -0,0 +1,19 @@
+package moni;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Testing {
+
+	public static void main(String[] args) {
+		 System.setProperty("webdriver.chrome.driver","./drivers/chromedriver.exe");
+		 ChromeDriver drivers = new ChromeDriver();
+
+
+		
+     
+
+          
+	}
+
+}
